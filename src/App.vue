621ba0d58b1a34108ca49684e0bd270678ba6e8a
baicy <baicy0118@gmail.com>
2019-09-07 16:35:14 +0800

    添加公共loading插件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <v-app>
-        <v-app-bar app dark>
-        <v-toolbar-title class="headline mr-5">
-            <router-link tag="span" to="/">
-                <span>Arknights Observer</span>
-                <span class="font-weight-light subtitle-2"> Demo</span>
-            </router-link>
-        </v-toolbar-title>
-        <v-toolbar-items>
-            <v-btn text router-link to="/">计划</v-btn>
-            <v-btn text router-link to="/character">练度</v-btn>
-            <v-btn text router-link to="/stock">仓库</v-btn>
-        </v-toolbar-items>
-    </v-app-bar>
     <v-content>
       <router-view/>
     </v-content>
@@ -29,11 +16,17 @@ export default {
   data: () => ({
     //
   }),
+  mounted() {
+    this.$loading.show();
+    var app = this;
+    document.addEventListener('readystatechange', function(){
+      if (document.readyState === 'complete') {
+        app.$loading.hide();
+      }
+    });
+  }
 };
 </script>
 
 <style>
-    .headline {
-        cursor: pointer;
-    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
 import router from './router'
+import loading from './plugins/loading'
+
+Vue.use(loading)
 
 Vue.config.productionTip = false
 
