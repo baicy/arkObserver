34b59660de2ae696e1a55388396c80c2394a3c14
baicy <baicy0118@gmail.com>
2020-04-14 16:04:15 +0800

    材料的完善

--- src/views/Material.vue
+++ src/views/Material.vue
@@ -1,47 +1,52 @@
 <template>
 <el-card class="workshop">
-  <div slot="header" class="pane-plan">
-    <div class="pane-material pane-need">
-      <div :class="[{ 'grid-0': i&&material.rarity<neededMaterials[i-1].rarity }]" v-for="(material,i) in neededMaterials" :key="i">
-        <el-popover
-          placement="bottom-start"
-          trigger="focus">
-          <div v-if="material.formula" class="popover-material">
-          <div class="formula">
-            <Material v-for="(c,i) in material.formula.costs" :key="i" :item="materials[indexes[c[0]]]" :number="c[1]" :store="stores[c[0]]" class="cost"></Material>
-          </div>
-          </div>
-          <div v-else>不可合成</div>
-          <Material slot="reference" class="material" :item="material" :number="needs[material.itemId]"></Material>
-        </el-popover>
+  <div slot="header">
+    <div class="pane-gold">合成所需龙门币: {{goldCost}}</div>
+    <div class="pane-plan">
+      <div class="pane-material pane-need">
+        <div :class="[{ 'grid-0': i&&material.rarity<neededMaterials[i-1].rarity }]" v-for="(material,i) in neededMaterials" :key="i">
+          <el-popover
+            placement="bottom-start"
+            trigger="focus">
+            <div v-if="material.formula" class="popover-material">
+            <div class="formula">
+              <Material v-for="(c,i) in material.formula.costs" :key="i" :item="materials[indexes[c[0]]]" :number="c[1]" :store="stores[c[0]]" class="cost"></Material>
+            </div>
+            </div>
+            <div v-else>不可合成</div>
+            <Material slot="reference" class="material" :item="material" :number="needs[material.itemId]"></Material>
+          </el-popover>
+        </div>
       </div>
+      <el-table
+        :data="planList"
+        header-row-class-name="row-dense"
+        :row-class-name="rowClass"
+        border
+        height="205"
+        class="list-plan">
+        <el-table-column width="90">
+          <template slot-scope="{row}">
+            <el-button @click="hideOrShowPlan(row)" class="icon-tool-hide" type="text"><i class="el-icon-view"></i></el-button>
+            <el-button @click="removePlan(row.id)" class="icon-tool-remove" type="text"><i class="el-icon-close"></i></el-button>
+            <el-button @click="completePlan(row)" :disabled="!completeds[row.id]" :class="{'icon-tool-complete': completeds[row.id]}" type="text"><i class="el-icon-check"></i></el-button>
+          </template>
+        </el-table-column>
+        <el-table-column prop="chara" label="干员" width="100" sortable></el-table-column>
+        <el-table-column prop="item" label="项目" width="180">
+          <template slot-scope="{row}">
+            {{ row.item ? (row.item+' '+row.from+'->'+row.to) : '' }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="materials" label="材料" width="380">
+          <template slot-scope="{row}">
+            <div class="materials">
+              <Material v-for="(m,i) in row.materials" :key="i" :item="materials[indexes[m[0]]]" :number="m[1]" class="material"></Material>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
-    <el-table
-      :data="planList"
-      :row-style="rowStyle"
-      border
-      height="140"
-      class="list-plan">
-      <el-table-column width="60">
-        <template slot-scope="{row}">
-          <i @click="hideOrShowPlan(row)" class="el-icon-view"></i>
-          <i @click="removePlan(row.id)" class="el-icon-minus"></i>
-        </template>
-      </el-table-column>
-      <el-table-column prop="chara" label="干员" width="100"></el-table-column>
-      <el-table-column prop="item" label="项目" width="180">
-        <template slot-scope="{row}">
-          {{row.item}} {{row.from}}->{{row.to}}
-        </template>
-      </el-table-column>
-      <el-table-column prop="materials" label="材料" width="380">
-        <template slot-scope="{row}">
-          <div style="display:flex;">
-            <Material v-for="(m,i) in row.materials" :key="i" :item="materials[indexes[m[0]]]" :number="m[1]"></Material>
-          </div>
-        </template>
-      </el-table-column>
-    </el-table>
   </div>
   <div class="toolbar-ground">
     <el-switch
@@ -50,7 +55,7 @@
     </el-switch>
   </div>
   <div class="pane-material pane-ground">
-    <div :class="['require', { 'grid-0': (material.type&&!groundMaterials[i-1].type)||(!material.type&&i&&material.rarity<groundMaterials[i-1].rarity) }, { 'needed': totalNeeds[material.itemId] }]" v-for="(material,i) in groundMaterials" :key="i">
+    <div :class="['require', { 'grid-0': (material.type=='book'&&!groundMaterials[i-1].type)||(material.type!='book'&&i&&(material.type!=groundMaterials[i-1].type||material.rarity<groundMaterials[i-1].rarity)) }, { 'needed': totalNeeds[material.itemId] }]" v-for="(material,i) in groundMaterials" :key="i">
       <el-popover
         class="name"
         placement="bottom-start"
@@ -61,11 +66,14 @@
             <Material v-for="(c,i) in material.formula.costs" :key="i" :item="materials[indexes[c[0]]]" :number="c[1]" :store="stores[c[0]]" class="cost"></Material>
           </div>
         </div>
-        <div v-else>不可合成</div>
+        <div>
+          <el-input-number v-model="extra" size="mini" :min="0" controls-position="right"></el-input-number>
+          <el-button @click="addExtraPlan(material.itemId)" type="primary" size="mini">extra</el-button>
+        </div>
         <Material slot="reference" class="material" :item="material" :show-number="false"></Material>
       </el-popover>
       <el-tag size="medium" type="primary" effect="dark">{{totalNeeds[material.itemId]}}</el-tag>
-      <el-input-number class="number" size="mini" v-model="requires[material.itemId]" :min="0" controls-position="right"></el-input-number>
+      <el-input-number v-model="requires[material.itemId]" :min="0" class="number" size="mini" controls-position="right"></el-input-number>
       <el-tag
         v-if="material.formula"
         :type="stores[material.itemId]+cals[material.itemId]<totalNeeds[material.itemId]?'primary':(cals[material.itemId]&&stores[material.itemId]+cals[material.itemId]>totalNeeds[material.itemId]?'danger':'info')"
@@ -75,7 +83,7 @@
         effect="dark">
           <i class="el-icon-s-tools"></i>
       </el-tag>
-      <el-input-number v-if="material.formula" class="number" size="mini" v-model="cals[material.itemId]" :min="0" :max="totalNeeds[material.itemId]" controls-position="right"></el-input-number>
+      <el-input-number v-if="material.formula" class="number" size="mini" v-model="cals[material.itemId]" :min="0" :max="totalNeeds[material.itemId]||Infinity" controls-position="right"></el-input-number>
       <el-tag
         :type="stores[material.itemId]+cals[material.itemId]>=totalNeeds[material.itemId]?'success':'danger'"
         class="tag-store"
@@ -91,8 +99,6 @@
 <script>
 import '@/assets/styles/material.less'
 import { material, book, chip } from '@/data/config/material.json'
-import characters from '@/data/source/character_table.json'
-import skills from '@/data/source/skill_table.json'
 import Material from '@/components/Material'
 
 export default {
@@ -101,12 +107,17 @@ export default {
     return {
       materials: [],
       indexes: {},
+      goldCost: 0,
       requires: {},
       cals: {},
       stores: {},
+      totalNeeds: {},
       ableMergeds: {},
-      onlyNeeded: false,
-      plans: []
+      onlyNeeded: true,
+      extra: 0,
+      plans: {},
+      planList: [],
+      completeds: {}
     }
   },
   computed: {
@@ -114,13 +125,18 @@ export default {
       let results = {};
       for(const m of this.materials) {
         this.$set(results, m.itemId, 0);
+        this.$set(this.totalNeeds, m.itemId, 0);
       }
+      this.goldCost = 0;
       for(const m of this.materials) {
         const need = results[m.itemId] + this.requires[m.itemId] - this.cals[m.itemId] - this.stores[m.itemId];
         this.$set(results, m.itemId, (need>0 ? need : 0));
+        this.$set(this.totalNeeds, m.itemId, this.totalNeeds[m.itemId] + this.requires[m.itemId]);
         if(!m.formula) continue;
+        this.goldCost += this.cals[m.itemId]*m.formula.goldCost;
         for(const cost of m.formula.costs) {
           this.$set(results, cost[0], results[cost[0]] + (this.cals[m.itemId] * cost[1]));
+          this.$set(this.totalNeeds, cost[0], this.totalNeeds[cost[0]] + (this.cals[m.itemId] * cost[1]));
         }
       } 
       return results;
@@ -128,45 +144,8 @@ export default {
     neededMaterials() {
       return this.materials.filter(m=>this.needs[m.itemId]).sort((a,b)=>b.rarity-a.rarity);
     },
-    totalNeeds() {
-      let results = {};
-      for(const m of this.materials) {
-        this.$set(results, m.itemId, 0);
-      }
-      for(const m of this.materials) {
-        this.$set(results, m.itemId, results[m.itemId] + this.requires[m.itemId]);
-        if(this.cals[m.itemId]>results[m.itemId]) {
-          this.$set(this.cals, m.itemId, results[m.itemId]);
-        }
-        if(!m.formula) continue;
-        for(const cost of m.formula.costs) {
-          this.$set(results, cost[0], results[cost[0]] + (this.cals[m.itemId] * cost[1]));
-        }
-      } 
-      return results;
-    },
     groundMaterials() {
-      return this.onlyNeeded ? this.materials.filter(m=>this.totalNeeds[m.itemId]) : this.materials;
-    },
-    planList() {
-      for(const i in this.materials) {
-        this.$set(this.requires, this.materials[i].itemId, 0);
-      }
-      let list = [];
-      for(const i in this.plans) {
-        if(!this.plans[i].hide) {
-          for(const m of this.plans[i].materials) {
-            this.$set(this.requires, m[0], this.requires[m[0]]+m[1]);
-          }
-        }
-        list.push(this.plans[i]);
-      }
-      list.sort((a,b)=>{
-        const la = a.hide?1:-1;
-        const lb = b.hide?1:-1;
-        return la-lb;
-      });
-      return list;
+      return this.onlyNeeded ? this.materials.filter(m=>this.totalNeeds[m.itemId]||this.cals[m.itemId]) : this.materials;
     }
   },
   watch:{
@@ -185,6 +164,12 @@ export default {
         this.$store.dispatch('setCal', newVal);
       },
       deep: true
+    },
+    plans: {
+      handler(newVal, oldVal) {
+        this.updatePlanList();
+      },
+      deep: true
     }
   },
   methods: {
@@ -200,13 +185,34 @@ export default {
         this.$set(this.cals, material.itemId, this.cals[material.itemId]-1);
       }
     },
-    rowStyle({row, rowIndex}) {
-      if(row.hide) {
-        return {
-          color: '#bfbfbf',
-          textDecoration: 'line-through'
-        }
+    updatePlanList() {
+      this.planList = [];
+      for(const i in this.materials) {
+        this.$set(this.requires, this.materials[i].itemId, 0);
       }
+      for(const i in this.plans) {
+        let completed = true;
+        for(const m of this.plans[i].materials) {
+          if(!this.plans[i].hide) {
+            this.$set(this.requires, m[0], this.requires[m[0]]+m[1]);
+          }
+          if(m[1]>this.stores[m[0]]) completed = false;
+        }
+        this.$set(this.completeds, i, completed);
+        this.planList.push(this.plans[i]);
+      }
+      this.planList.sort((a,b)=>{
+        const la = a.hide?1:-1;
+        const lb = b.hide?1:-1;
+        return la-lb;
+      });
+    },
+    rowClass({row, rowIndex}) {
+      let classes = ['row-dense'];
+      if(row.hide) {
+        classes.push('row-hide');
+      }
+      return classes;
     },
     hideOrShowPlan(row) {
       row.hide = !row.hide;
@@ -217,6 +223,26 @@ export default {
     },
     removePlan(id) {
       this.$store.dispatch('removePlan', id);
+    },
+    completePlan(row) {
+      for(const m of row.materials) {
+        this.$set(this.stores, m[0], this.stores[m[0]]-m[1]);
+      }
+      this.$store.dispatch('removePlan', row.id);
+    },
+    addExtraPlan(mid) {
+      let plan = {
+        id: 'extra'+new Date().getTime(),
+        chara: '',
+        item: '',
+        hide: false,
+        materials: [[mid, this.extra]]
+      };
+      this.$store.dispatch('setPlan', {
+        id: plan.id,
+        data: plan
+      });
+      this.extra = 0;
     }
   },
   created() {
